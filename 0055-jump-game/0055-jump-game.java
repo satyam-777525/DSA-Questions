bdfa138c7368dff6bd20b19206097efe8e061a75
Satyam Varshney <sv1061211@gmail.com>
2026-03-23 22:33:32 +0530

    Time: 2 ms (85.54%), Space: 47.1 MB (99.61%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int high=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>high) return false;
+            high=Math.max(high,i+nums[i]);
+            if(high>=nums.length-1) return true;
+        }
+        return false;
+        
+    }
+}
